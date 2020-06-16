c8cd253297e7df5db60e274d796a4a92a34c4d4a
skysoul19850730 <kyzy_jk001@163.com>
2020-06-16 10:56:05 +0800

    test2

--- app/src/main/java/com/jscoolstar/accountremeber/activities/register/models/RegisterModel.kt
+++
@@ -1,9 +0,0 @@
-package com.jscoolstar.accountremeber.activities.register.models
-
-interface RegisterModel {
-
-    fun isUserNameExsits(userName:String):Boolean
-
-    fun addUser(userName: String,password: String,passwordTip: String?): Long
-
-}
